4af0350b03e0fc393c4c9c591a1b0ee4cc573b37
CBSW-ConsultRed <christopher.whitting@consult.red>
2024-03-06 16:14:24 +0000

    Added a jest-style syntax example

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -2,14 +2,6 @@
 
 CBSW_UNIT_SUITE("Test Suite") {
     CBSW_UNIT_FIXTURE("This is a test fixture") {
-        CBSW_UNIT_BEFORE_EACH() {
-
-        }
-
-        CBSW_UNIT_AFTER_EACH() {
-
-        }
-
         CBSW_UNIT_CASE("This is a failing scenario") {
             CBSW_UNIT_ASSERT(1 == 0);
         }
