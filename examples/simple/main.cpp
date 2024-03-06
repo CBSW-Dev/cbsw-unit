b35240896c6901f3aec1c7bee4286dfadf20a467
CBSW-ConsultRed <christopher.whitting@consult.red>
2024-03-06 15:31:53 +0000

    Added BeforeEach

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -2,6 +2,10 @@
 
 CBSW_UNIT_SUITE("Test Suite") {
     CBSW_UNIT_FIXTURE("This is a test fixture") {
+        CBSW_UNIT_BEFORE_EACH() {
+
+        }
+
         CBSW_UNIT_CASE("This is a failing scenario") {
             CBSW_UNIT_ASSERT(1 == 0);
         }
